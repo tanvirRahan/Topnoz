3d7b83186697f3b9cdeb0c933b8b624f9745c7bb
Tanvir Rahan Rifat <tanvirrahanrifat@gmail.com>
2025-06-11 18:33:57 +0600

    Update settings.py

--- petCommerce/settings.py
+++ petCommerce/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2!(x(n4gdq7l=&ib+)(khyxt^mpl_q)=88k-l0b*_&=8jp2wic'
 
 # Production এ অবশ্যই False সেট করতে হবে
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'topnoz.com',

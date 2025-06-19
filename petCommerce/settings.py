9fe06051ad19d42536e15e697c6672df52bf1436
Tanvir Rahan Rifat <tanvirrahanrifat@gmail.com>
2025-06-20 03:58:37 +0600

    Update settings.py

--- petCommerce/settings.py
+++ petCommerce/settings.py
@@ -105,6 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+LOGIN_URL = '/userLogin/'
 # ─────────────────────────────────────────────────────────────────────────────
 # INTERNATIONALIZATION
 # ─────────────────────────────────────────────────────────────────────────────

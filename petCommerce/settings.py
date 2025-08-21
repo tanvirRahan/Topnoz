695d226ae938e9ee39edc7d5a26ada2163521f9a
Tanvir Rahan Rifat <159823957+TRR-Rifat@users.noreply.github.com>
2025-08-22 00:28:18 +0600

    sqllite add

--- petCommerce/settings.py
+++ petCommerce/settings.py
@@ -67,7 +67,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'petCommerce.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+    #'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))
+
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
 
 AUTH_PASSWORD_VALIDATORS = [
